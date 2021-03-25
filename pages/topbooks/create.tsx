a93e88567ea9e93c0ad2e3e189760484b49c5835
DevDohz <dev@dohz.net>
2021-03-25 13:51:53 +0100

    Delete Review and TopBook

--- pages/topbooks/create.tsx
+++
@@ -1,6 +0,0 @@
-import { NextComponentType, NextPageContext } from "next";
-import { Form } from "../../components/topbook/Form";
-
-const Page: NextComponentType<NextPageContext> = () => <Form />;
-
-export default Page;
